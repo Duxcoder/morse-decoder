b9a85d970d2c6fb7a95628c538d702b06a986e67
Duxcoder <iinegve_25@mail.ru>
2023-01-05 22:32:55 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -36,10 +36,69 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
-function decode(expr) {
-    // write your solution here
+function decode (expr) {
+    return expr.split('**********')
+    .map(encodeWord => divider(encodeWord, 10)
+        .map(encodeLetter => divider(encodeLetter.slice(encodeLetter.indexOf('1')), 2) // remove padding zero
+            .map(number => decoderToMorse(number)).join(''))
+                .map(morse => decoderToChar(morse)).join('')).join(' ')
 }
+
+const divider = (numberStr, amount) => {
+    const arr = [];
+    const slicer = [];
+    for (let i = 0; i < (numberStr.length / amount); i++) {
+        if (i === 0) {
+            arr[i] = numberStr.substr(0, amount)
+            slicer[i] = numberStr.slice(amount)
+        } else {
+            arr[i] = slicer[i-1].substr(0, amount)
+            slicer[i] = slicer[i-1].slice(amount)
+        }
+    }
+    return arr
+}
+
+const decoderToMorse = (numbers) => {
+    if (numbers === '10') return '.'
+    if (numbers === '11') return '-'
+}
+
+const decoderToChar = (morse) => {
+    console.log(morse)
+    for (let key in MORSE_TABLE) {
+        if (morse === key) return MORSE_TABLE[key]
+    }
+}
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"))
+
+// It's my mistake, i create encoder...
+// const replaceMorse = (letter) => {
+//     for (let key in MORSE_TABLE) {
+//         if (letter === MORSE_TABLE[key]) return key
+//     }
+// }
+// const createMorseStr = (expr) => {
+//     return expr.split(' ').map(word => word.split('').map(letter => replaceMorse(letter)));
+// }
+
+// const addingZero = (encodedStr) => {
+//     while (encodedStr.length < 10){
+//         encodedStr = '0' + encodedStr
+//     }
+//     return encodedStr
+// }
+// function encode(expr) {
+//     return createMorseStr(expr).map(word => {
+//         return word.map(letter => letter.split('').map(char => {
+//             if (char === '-') return '11'
+//             if (char === '.') return '10'
+//             return char
+//         }).join('')).join('')
+//     }).map(word => {
+//         return addingZero(word)
+//     }).join('**********')
+// }
 
 module.exports = {
     decode
